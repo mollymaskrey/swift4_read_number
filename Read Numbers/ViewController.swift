ac42855e6d559ad67e7c9d0b5f233865aae8e355
Molly Maskrey <mollymaskrey@gmail.com>
2017-08-23 14:53:10 -0600

    Initial Commit

---
+++ Read Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Read Numbers
+//
+//  Created by Molly Maskrey on 8/14/17.
+//  Copyright © 2017 MollyMaskrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
